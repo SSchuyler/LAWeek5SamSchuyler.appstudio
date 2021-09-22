2572df985dabb3f455483d15e10081cfd83da065
Samuel Schuyler <sjs08496@creighton.edu>
2021-09-22 15:54:58 -0500

    While

---
+++ forms/For/For.js
@@ -0,0 +1,16 @@
+/*
+Second program
+Scenario: you want a program that outputs names to the console, one name per line.
+Requirements
+Name a form 'for'.
+Use a for loop.
+Note: see if you can do this using a for loop with only one line of code in it. 
+Create an array named 'names' that holds this data:
+
+Bob, Janet, Tom, Erin, Lucy, Alyssa, Dustin
+Each time the loop runs, it must output a name to the console. So eventually your program will have output each name, one name per line.
+Bob
+Janet
+Tom
+etc. 
+*/
